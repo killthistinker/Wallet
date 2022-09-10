8a0b1e9d46c3626243a08e206bdb2d2f8b1528e0
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 15:41:06 +0600

    #1 подключил базу

---
+++ Wallet/Data/WalletContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Wallet.Models;
+
+namespace Wallet.Data
+{
+    public class WalletContext : IdentityDbContext<User, Role, int>
+    {
+      
+        public WalletContext(DbContextOptions<WalletContext> options) : base(options)
+        {
+            
+        }
+    }
+}
