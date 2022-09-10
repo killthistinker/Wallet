a6f4b748fc0dc2b5dff9c0cd2440ee190dccc084
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 16:48:56 +0600

    #2 Реализация паттерна "Репозиторий"
    ***
    Добаил сервис для юзера
    ***

---
+++ Wallet/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Wallet.Models;
+
+namespace Wallet.Repository.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    { 
+        public Task<User> GetFirstOrDefaultByIdAsync(int id);
+        public Task<User> GetFirstOrDefaultByNameAsync(string name);
+        public Task<User> GetFirstOrDefaultByUserIdAsync(int id);
+    }
+}
