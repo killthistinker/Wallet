e59d969a668b59502b8ead2ba38f282ef23311ef
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:24:52 +0600

    #3 Добавил контроллер для добавления баланса

---
+++ Wallet/Controllers/BalanceController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Wallet.Models;
+using Wallet.Models.ViewModels;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Controllers
+{
+    public class BalanceController : Controller
+    {
+        private readonly IBalanceService _balanceService;
+        private readonly UserManager<User> _userManager;
+
+        public BalanceController(IBalanceService balanceService, UserManager<User> userManager)
+        {
+            _balanceService = balanceService;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetBalance(string userName)
+        {
+            decimal userBalance = await _balanceService.GetBalance(userName);
+            return Json(userBalance);
+        }
+
+       
+    }
+}
