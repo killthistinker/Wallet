3cc29fdb1d78a641f38a5528138d27a54864f26b
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:48:24 +0600

    #4 Добавил репозиторий для транзакций

---
+++ Wallet/Repository/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Wallet.Models;
+
+namespace Wallet.Repository.Interfaces
+{
+    public interface ITransactionRepository : IRepository<Transaction>
+    {
+        public IEnumerable<Transaction> GetUserTransactionsById(int userId);
+    }
+}
---
+++ Wallet/Repository/TransactionRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wallet.Data;
+using Wallet.Models;
+using Wallet.Repository.Interfaces;
+
+namespace Wallet.Repository
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly WalletContext _db;
+
+        public TransactionRepository(WalletContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Transaction> GetAll() => _db.Transactions;
+
+        public void Create(Transaction item) => _db.Transactions.Add(item);
+
+        public void Update(Transaction item) => _db.Transactions.Update(item);
+
+        public void Remove(Transaction item) => _db.Transactions.Remove(item);
+
+        public async Task SaveAsync() => await _db.SaveChangesAsync();
+
+        public IEnumerable<Transaction> GetUserTransactionsById(int userId) =>
+            _db.Transactions.Where(u => u.CurrentUserId == userId);
+    }
+}
