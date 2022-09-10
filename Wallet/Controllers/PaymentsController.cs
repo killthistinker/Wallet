cda0d499da1afd31ceb000950c1e025ce61489d4
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 18:14:10 +0600

    #6 Добавил контроллер для платежей

---
+++ Wallet/Controllers/PaymentsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Wallet.Models;
+using Wallet.Models.ViewModels;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly IPaymentsService _service;
+        private readonly UserManager<User> _userManager;
+        
+        public PaymentsController(IPaymentsService service, UserManager<User> userManager)
+        {
+            _service = service;
+            _userManager = userManager;
+        }
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var serviceProviders = _service.GetAll();
+            return View(serviceProviders);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> NewPayment([FromBody]AddPaymentViewModel model)
+        {
+            int.TryParse(_userManager.GetUserId(User), out int userId);
+            int statusCode = await _service.AddPayment(model,userId);
+            return Ok(statusCode);
+        }
+    }
+}
