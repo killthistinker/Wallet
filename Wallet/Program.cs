8e9fa480730cd4f774a264ee5dd807aae5edd2c6
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 15:43:30 +0600

    #1 Настроил инициализацию ролей

---
+++ Wallet/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Wallet.Models;
+using Wallet.Services;
+
+namespace Wallet
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var rolesManager = services.GetRequiredService<RoleManager<Role>>();
+                await RoleInitializer.SeedARoleUser(rolesManager);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured while seeding the database.");
+            }
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
---
+++ Wallet/Services/RoleInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Wallet.Models;
+
+namespace Wallet.Services
+{
+    public class RoleInitializer
+    {
+        public static async Task SeedARoleUser(RoleManager<Role> roleManager)
+        {
+            var roles = new[] { "user", "admin" };
+
+            foreach (var role in roles)     
+            {
+                if (await roleManager.FindByNameAsync(role) is null)
+                {
+                    await roleManager.CreateAsync(new Role{Name = role});
+                }
+            }
+        }
+    }
+}
