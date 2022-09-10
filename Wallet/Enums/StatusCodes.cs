451c5aca25ab19e30dfe6e4e3798c190fcf53d75
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:59:04 +0600

    #5 Добавил enum для статус кодов

---
+++ Wallet/Enums/StatusCodes.cs
@@ -0,0 +1,13 @@
+﻿namespace Wallet.Enums
+{
+    public enum StatusCodes
+    {
+        Success = 200,
+        Error = 500,
+        EntityNotFound = 400,
+        UserNotFound = 404,
+        InsufficientFunds = 406,
+        ReplenishYourself = 407,
+        NegativeBalance = 409
+    }
+}
