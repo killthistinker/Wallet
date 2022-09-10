15260e4670a4d899413dbf4b133e5c5efc783de0
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:49:53 +0600

    Рефакторинг кода

---
+++ Wallet/Helpers/ModelStateHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Wallet.Helpers
+{
+    public static class ModelStateHelper
+    {
+        public static void AddErrors(this ModelStateDictionary self, List<string> errors)
+        {
+            errors.ForEach(e =>
+            {
+                self.AddModelError(string.Empty, e);
+            });
+        }
+    }
+}
