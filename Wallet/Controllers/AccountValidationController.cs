6c90ce62d0dbc9fe69d60ac4da3006940ebf67dc
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 15:42:52 +0600

    #1 Добавил контроллер для валидации пользваотеля

---
+++ Wallet/Controllers/AccountValidationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Wallet.Data;
+
+namespace Wallet.Controllers
+{
+    public class AccountValidationController : Controller
+    {
+        private readonly WalletContext _context;
+
+        public AccountValidationController(WalletContext context)
+        {
+            _context = context;
+        }
+
+        public bool CheckAccountEmail(string email)
+        {
+            bool validation = !_context.Users.AsEnumerable().Any(p => p.Email.Equals(email));
+            return validation;
+        }
+
+        public bool CheckAccountName(string userName)
+        {
+            bool validation = !_context.Users.AsEnumerable().Any(p => p.UserName.Equals(userName));
+            return validation;
+        }
+    }
+}
