b99c8c44b6986686542940074cd2340f0810f0b4
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 15:40:19 +0600

    #1 Добавил модель юзера

---
+++ Wallet/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Wallet.Models
+{
+    public class User : IdentityUser<int>
+    {
+        public decimal Balance { get; set; }
+        public int AuthorizationId { get; set; }
+        public List<Transaction> Transactions { get; set; }
+
+        public User()
+        {
+            Transactions = new List<Transaction>();
+        }
+    }
+}
