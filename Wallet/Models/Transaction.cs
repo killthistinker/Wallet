1edde2ed032dd22767acb024b80042dc59f11952
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:48:58 +0600

    #4 Создал сущность с транзакциями

---
+++ Wallet/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Wallet.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Sum { get; set; }
+        public string Type { get; set; }
+        public string Sender { get; set; }
+        public string Addressee { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int CurrentUserId { get; set; }
+        public User CurrentUser { get; set; }
+    }
+}
