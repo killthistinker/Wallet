26067aa46056c0d11b5b97d99885d9c60e7a580c
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 15:40:33 +0600

    #1 Добавил маппер для регистрации юзера

---
+++ Wallet/Helpers/UserRegistrationMapper.cs
@@ -0,0 +1,19 @@
+﻿using Wallet.Models;
+using Wallet.Models.ViewModels;
+
+namespace Wallet.Helpers
+{
+    public static class UserRegistrationMapper
+    {
+        public static User MapToUser(this RegistrationViewModel self)
+        {
+            User user = new User()
+            {
+                Email = self.Email,
+                UserName = self.UserName,
+                Balance = 10000
+            };
+            return user;
+        }
+    }
+}
