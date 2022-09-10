b7dd1c43bf21bf57f77c0ccf4824e52db346c06f
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 18:13:43 +0600

    #6 Реализовал паттерн для новых моделей

---
+++ Wallet/Repository/Interfaces/IServiceProviderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wallet.Models;
+
+namespace Wallet.Repository.Interfaces
+{
+    public interface IServiceProviderRepository : IRepository<ServiceProvider>
+    {
+        public Task<ServiceProvider> GetProviderFirsOrDefaultFromIdAsync(int providerId);
+    }
+}
---
+++ Wallet/Repository/ServiceProviderRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Wallet.Data;
+using Wallet.Models;
+using Wallet.Repository.Interfaces;
+
+namespace Wallet.Repository
+{
+    public class ServiceProviderRepository : IServiceProviderRepository
+    {
+        private readonly WalletContext _db;
+
+        public ServiceProviderRepository(WalletContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<ServiceProvider> GetAll() => _db.ServiceProviders;
+
+        public void Create(ServiceProvider item) => _db.ServiceProviders.Add(item);
+
+        public void Update(ServiceProvider item) => _db.ServiceProviders.Update(item);
+
+        public void Remove(ServiceProvider item) => _db.ServiceProviders.Remove(item);
+
+        public async Task SaveAsync() => await _db.SaveChangesAsync();
+
+        public async Task<ServiceProvider> GetProviderFirsOrDefaultFromIdAsync(int providerId) =>
+         await _db.ServiceProviders.Include(p => p.UsersInfos).FirstOrDefaultAsync(p => p.Id == providerId);
+    }
+}
