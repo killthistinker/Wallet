ca439d3a8df5d66767020e394c4d4edff259a611
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 16:47:32 +0600

    #2 Создал интерфейс для сервиса авторизации

---
+++ Wallet/Services/Abstractions/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Wallet.Models.ViewModels;
+using IdentityResult = Wallet.DataObjects.IdentityResult;
+
+namespace Wallet.Services.Abstractions
+{
+    public interface IAccountService
+    {
+        public Task<IdentityResult> Register(RegistrationViewModel model);
+        public Task<IdentityResult> LogIn(LoginViewModel model);
+        public Task LogOf();
+        public Task<int> GenerateId();
+    }
+}
