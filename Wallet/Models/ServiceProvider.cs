55f296828842af624e826c15a3229a570ab27a9c
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 18:12:44 +0600

    #6 Добавил модели для платежей

---
+++ Wallet/Models/ServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Wallet.Models
+{
+    public class ServiceProvider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public List<UserInfo> UsersInfos { get; set; }
+
+        public ServiceProvider()
+        {
+            UsersInfos = new List<UserInfo>();
+        }
+    }
+}
---
+++ Wallet/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Wallet.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public int PropsId { get; set; }
+        public decimal Balance { get; set; }
+        public int ServiceProviderId { get; set; }
+        public ServiceProvider ServiceProvider { get; set; }
+    }
+}
