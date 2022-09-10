c43920535f3f5bcd2dc39979c5b8f3d13d72e65a
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:26:47 +0600

    #3 Добавил сервис для пополнения баланса

---
+++ Wallet/Services/Abstractions/IBalanceService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Wallet.Models.ViewModels;
+
+namespace Wallet.Services.Abstractions
+{
+    public interface IBalanceService
+    {
+        public Task<decimal> GetBalance(string userName);
+        public Task<int> UserTransaction(SetBalanceViewModel setBalance, int userId);
+        public Task<bool> UpBalance(SetBalanceViewModel setBalance);
+    }
+}
---
+++ Wallet/Services/BalanceService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Wallet.Enums;
+using Wallet.Models;
+using Wallet.Models.ViewModels;
+using Wallet.Repository.Interfaces;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Services
+{
+    public class BalanceService : IBalanceService
+    {
+       private readonly IUserRepository _repository;
+
+        public BalanceService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<decimal> GetBalance(string userName)
+        {
+            var user = await _repository.GetFirstOrDefaultByNameAsync(userName);
+            if (user is null) return -1;
+            return user.Balance;
+        }
+
+        
+    }
+}
