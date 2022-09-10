69d880486fc7184cd3fb063c48e2843ee57e1474
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 18:11:30 +0600

    #5 Создать модель для платежей

---
+++ Wallet/Models/ViewModels/AddPaymentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Wallet.Models.ViewModels
+{
+    public class AddPaymentViewModel
+    {
+        public int ProviderId { get; set; }
+        public decimal Sum { get; set; }
+        public int PropsId { get; set; }
+    }
+}
