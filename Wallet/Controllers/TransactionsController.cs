93bed32e0308c423e15f85f999a4024ec4369fdf
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:47:41 +0600

    #4 Добавил контроллер для транзакций

---
+++ Wallet/Controllers/TransactionsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Wallet.Models;
+using Wallet.Models.ViewModels;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ITransactionsService _service;
+
+        public TransactionsController(UserManager<User> userManager, ITransactionsService service)
+        {
+            _service = service;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            int.TryParse(_userManager.GetUserId(User), out int userId);
+            TransactionsViewModel transactions = _service.GetUserTransactionById(userId);
+            return View(transactions);
+        }
+    }
+}
