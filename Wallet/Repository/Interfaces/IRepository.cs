87d4081dc38b99b95d862a6734aff530fca0d120
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 16:50:46 +0600

    #2 Добавил обобщенный интерфейс с основными методами

---
+++ Wallet/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Wallet.Repository.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public IEnumerable<T> GetAll();
+        public void Create(T item);
+        public void Update(T item);
+        public void Remove(T item);
+        public Task SaveAsync();
+    }
+}
