76180d0f9177b91efd40559fee1589bf405424f1
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:31:16 +0600

    #3 Добавил сраницу для отображения формы с пополнением баланса

---
+++ Wallet/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Wallet.Models;
+using Wallet.Repository.Interfaces;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IUserService _service;
+        public HomeController( UserManager<User> userManager, IUserService service)
+        {
+            _userManager = userManager;
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            int.TryParse(_userManager.GetUserId(User), out int userId);
+            var model = await _service.GetUser(userId);
+            return View(model);
+        }
+    }
+}
