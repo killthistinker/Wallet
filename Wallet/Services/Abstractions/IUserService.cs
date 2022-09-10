71eb6ada682ec425f0c4505ae2ad7cbc121d6c2c
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 16:49:40 +0600

    #2 Реализация паттерна "репозиторий"
    ***
    Добавил интерфейс для юзера
    ***

---
+++ Wallet/Services/Abstractions/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Wallet.Models.ViewModels;
+
+namespace Wallet.Services.Abstractions
+{
+    public interface IUserService
+    {
+        public Task<UserViewModel> GetUser(int id);
+    }
+}
