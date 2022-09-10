60dfdd8b011cf9b74e082e57154ab68970377615
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 15:39:19 +0600

    #1 Создал контроллер для аккаунтов
    ****
    Настроил меетоды регистрации, авторизации, выхода и аутентификации

---
+++ Wallet/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wallet.Helpers;
+using Wallet.Models.ViewModels;
+using Wallet.Services.Abstractions;
+using StatusCodes = Wallet.Enums.StatusCodes;
+
+
+namespace Wallet.Controllers
+{
+     public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+       
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegistrationViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var result = await _accountService.Register(model);
+            if (result.StatusCodes == StatusCodes.Success)
+                return RedirectToAction("Index", "Home");
+            
+            if(result.ErrorMessages.Any())
+                ModelState.AddErrors(result.ErrorMessages);
+
+            return View(model);
+        }
+        
+        [HttpGet]
+        public IActionResult LogIn(string returnUrl = null)
+        {
+            return View(new LoginViewModel {ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogIn(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = await _accountService.LogIn(model);
+            if (result.StatusCodes == StatusCodes.Success)
+            {
+                if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    return Redirect(model.ReturnUrl);
+                return RedirectToAction("Index", "Home");
+            }
+            
+            if (result.ErrorMessages.Any())
+                ModelState.AddErrors(result.ErrorMessages);
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            await _accountService.LogOf();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
