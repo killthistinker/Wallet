11c60bd8b01f9c6f2eb1d3f5c8e795c8c803e95b
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:48:02 +0600

    #4 Настроил сервис для транзакций

---
+++ Wallet/Services/Abstractions/ITransactionsService.cs
@@ -0,0 +1,9 @@
+﻿using Wallet.Models.ViewModels;
+
+namespace Wallet.Services.Abstractions
+{
+    public interface ITransactionsService
+    {
+        public TransactionsViewModel GetUserTransactionById(int userId);
+    }
+}
---
+++ Wallet/Services/TransactionsService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wallet.Models;
+using Wallet.Models.ViewModels;
+using Wallet.Repository.Interfaces;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Services
+{
+    public class TransactionsService : ITransactionsService
+    {
+        private readonly ITransactionRepository _db;
+
+        public TransactionsService(ITransactionRepository db)
+        {
+            _db = db;
+        }
+
+        public TransactionsViewModel GetUserTransactionById(int userId)
+        {
+            IEnumerable<Transaction> transactions = _db.GetUserTransactionsById(userId);
+            if (transactions is null) return null;
+            
+            TransactionsViewModel model = new TransactionsViewModel()
+            {
+                Transactions = transactions
+            };
+            return model;
+        }
+    }
+}
