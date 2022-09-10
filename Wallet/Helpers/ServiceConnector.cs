ff6a5c315ad759ead452ce82337f820786f4b61a
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:29:09 +0600

    #2 Реализовал ServiceConnector

---
+++ Wallet/Helpers/ServiceConnector.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Wallet.Repository;
+using Wallet.Repository.Interfaces;
+using Wallet.Services;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Helpers
+{
+    public static class ServiceConnector
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<IServiceProviderRepository, ServiceProviderRepository>();
+            services.AddScoped<IUserInfoRepository, UserInfoRepository>();
+            services.AddTransient<ITransactionsService, TransactionsService>();
+            services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IBalanceService, BalanceService>();
+            services.AddTransient<IPaymentsService, PaymentsService>();
+            return services;
+        }
+    }
+}
