18111726a9c4d4d1491e18a431dfab4a68ee8112
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 18:12:14 +0600

    #6 Настроил сервис для платежей

---
+++ Wallet/Services/Abstractions/IPaymentsService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Wallet.Models;
+using Wallet.Models.ViewModels;
+
+namespace Wallet.Services.Abstractions
+{
+    public interface IPaymentsService
+    {
+        public PaymentViewModel GetAll();
+        public Task<int> AddPayment(AddPaymentViewModel model, int userId);
+        public Task AddTransaction(User user, ServiceProvider paymentsService, UserInfo paymentUser, decimal sum);
+    }
+}
---
+++ Wallet/Services/PaymentsService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Wallet.Enums;
+using Wallet.Models;
+using Wallet.Models.ViewModels;
+using Wallet.Repository.Interfaces;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Services
+{
+    public class PaymentsService : IPaymentsService
+    {
+        private readonly IServiceProviderRepository _providerRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IUserInfoRepository _userInfoRepository;
+
+        public PaymentsService(IServiceProviderRepository repository, IUserRepository userRepository, IUserInfoRepository userInfoRepository)
+        {
+            _providerRepository = repository;
+            _userRepository = userRepository;
+            _userInfoRepository = userInfoRepository;
+        }
+
+        public PaymentViewModel GetAll()
+        {
+            PaymentViewModel model = new PaymentViewModel
+            {
+                ServiceProviders = _providerRepository.GetAll()
+            };
+            return model;
+        }
+
+        public async Task<int> AddPayment(AddPaymentViewModel model, int userId)
+        {
+            if (model.Sum <= 0) return (int) StatusCodes.NegativeBalance;
+            
+            var userSender = await _userRepository.GetFirstOrDefaultByIdAsync(userId);
+            if (userSender is null) return (int) StatusCodes.UserNotFound;
+            
+            var payment = await _providerRepository.GetProviderFirsOrDefaultFromIdAsync(model.ProviderId);
+            if (payment is null) return (int) StatusCodes.UserNotFound;
+            
+            var paymentUser = payment.UsersInfos.FirstOrDefault(u => u.PropsId == model.PropsId);
+            if (paymentUser is null) return (int) StatusCodes.UserNotFound;
+            if (userSender.Balance < model.Sum) return (int) StatusCodes.InsufficientFunds;
+            
+            userSender.Balance -= model.Sum;
+            payment.Balance += model.Sum;
+            paymentUser.Balance += model.Sum;
+            
+            await AddTransaction(userSender, payment, paymentUser, model.Sum);
+            return (int) StatusCodes.Success;
+        }
+
+        public async Task AddTransaction(User user, ServiceProvider paymentsService, UserInfo paymentUser, decimal sum)
+        {
+            Transaction senderTransaction = new Transaction
+            {
+                Sum = sum,
+                Type = "Платеж",
+                Sender = user.AuthorizationId.ToString(),
+                Addressee = paymentsService.Name,
+                TransactionDate = DateTime.Now,
+                CurrentUserId = user.Id
+            };
+            user.Transactions.Add(senderTransaction);
+            _userRepository.Update(user);
+            _userInfoRepository.Update(paymentUser);
+            _providerRepository.Update(paymentsService);
+            await _userRepository.SaveAsync();
+            await _providerRepository.SaveAsync();
+            await _userInfoRepository.SaveAsync();
+        }
+    }
+}
