96a0a5faf82b52098fdfd78cf3a6cd8f0def1dce
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 17:59:31 +0600

    #5 Рефакторинг кода

---
+++ Wallet/Startup.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Wallet.Data;
+using Wallet.Helpers;
+using Wallet.Models;
+
+namespace Wallet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<WalletContext>(ops => ops.UseNpgsql(connection))
+                .AddIdentity<User, Role>(
+                    options =>
+                    {
+                        options.Password.RequiredLength = 5;
+                        options.Password.RequireNonAlphanumeric = false;
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                        options.Password.RequireDigit = false;
+                    }).AddEntityFrameworkStores<WalletContext>().AddDefaultTokenProviders();
+            services.AddApplicationServices(Configuration);
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Wallet/wwwroot/js/site.js
@@ -0,0 +1,166 @@
+﻿
+$(document).ready(function () {
+    $("#password-view").on("click", function () {
+        var type = $("#password").attr('type') === "text" ? "password" : "text";
+        $("#password").prop('type', type);
+    });
+
+    $("#confirm-view").on("click", function () {
+        var type = $("#confirm").attr('type') == "text" ? "password" : "text";
+        $("#confirm").prop('type', type);
+    });
+
+    $("#password-view-login").on("click", function () {
+        var type = $("#login-password").attr('type') == "text" ? "password" : "text";
+        $("#login-password").prop('type', type);
+    });
+    var userName = $('#get-user-name').html();
+    function GetUserBalance(){
+        $.ajax({
+            method: "GET",
+            url: 'https://localhost:5001/balance/getBalance',
+            data: { userName: userName },
+            contentType: 'application/json',
+            success: function (res) {
+                console.log(res)
+                $('#user-balance').html(`Ваш баланс: ${res} `);
+            }
+        });
+    }
+    GetUserBalance();
+    function updateUserBalance(){
+        $.ajax({
+            method: "GET",
+            url: 'https://localhost:5001/balance/getBalance',
+            data: { userName: userName },
+            contentType: 'application/json',
+            success: function (res) {
+                console.log(res)
+                $('#balance-title').html(`Ваш баланс: ${res} `);
+                $('#user-balance').html(`Ваш баланс: ${res} `);
+            }
+        });
+    }
+    $('#form-balance').submit(function (e) {
+        e.preventDefault();
+        var userId = $('#user-id').val();
+        var balanceUp = $('#balance-data').val();
+        var balance = $('#balance-up');
+        var setBalance = { userId: userId, balance: balanceUp }
+        $.ajax({
+            method: "POST",
+            url: 'https://localhost:5001/balance/upbalance',
+            data: JSON.stringify(setBalance),
+            contentType: 'application/json',
+            success: function (res) {
+                if (res === 200) {
+                    console.log(res)
+                    balance.attr('data-content', `Баланс успешно пополнен на сумму: ${balanceUp}`);
+                    balance.popover('show')
+                }
+                else {
+                    balance.attr('data-content', `Введены некорректные данные`);
+                    balance.popover('show');
+                }
+            }
+        });
+    });
+    $('#form-balance-user').submit(function (e) {
+        e.preventDefault();
+        var userId = $('#user-id').val();
+        var balanceUp = $('#balance-data').val();
+        var formInformer = $('#balance-up');
+        console.log(userId);
+        console.log(balanceUp);
+        var setBalance = {
+            userId: userId,
+            balance: balanceUp
+        }
+        $.ajax({
+            method: "POST",
+            url: 'https://localhost:5001/balance/usertransaction',
+            data: JSON.stringify(setBalance),
+            contentType: 'application/json',
+            success: function (res) {
+                switch (res) {
+                    case 200:
+                        formInformer.attr('data-content', `Баланс пользователя: ${userId} пополнен на сумму: ${balanceUp}`);
+                        formInformer.popover('show');
+                        break;
+                    case 406:
+                        formInformer.attr('data-content', `Недостаточно средств`);
+                        formInformer.popover('show')
+                        break;
+                    case 404:
+                        formInformer.attr('data-content', `пользователь не найден`);
+                        formInformer.popover('show');
+                        break;
+                    case 407:
+                        formInformer.attr('data-content', `нельзя пополнить баланс самому себе`);
+                        formInformer.popover('show');
+                        break;
+                    case 409:
+                        formInformer.attr('data-content', `введена отрицательная сумма`);
+                        formInformer.popover('show');
+                        break;
+                    default:
+                        formInformer.attr('data-content', `Введены некорректные данные`);
+                        formInformer.popover('show');
+                        break;
+                }
+                updateUserBalance();
+            }
+        });
+    });
+
+    $('#payment-form').submit(function (e) {
+        e.preventDefault();
+        var providerId = $('#provider').val();
+        var providerName = $('#provider').val();
+        var props = $('#props').val();
+        var paymentSum = $('#payment-sum').val();
+        var paymentInformer = $('#payment-button');
+        var addPaymentViewModel = {
+            providerId: providerId,
+            sum: paymentSum,
+            propsId: props
+        }
+        console.log(addPaymentViewModel, providerName);
+        $.ajax({
+            method: "POST",
+            url: 'https://localhost:5001/payments/newpayment',
+            data: JSON.stringify(addPaymentViewModel),
+            contentType: 'application/json',
+            success: function (res) {
+                switch (res) {
+                    case 200:
+                        paymentInformer.attr('data-content', `Платеж поставщику ${providerName} Совершен на сумму: ${paymentSum}`);
+                        paymentInformer.popover('show');
+                        break;
+                    case 406:
+                        paymentInformer.attr('data-content', `Недостаточно средств`);
+                        paymentInformer.popover('show')
+                        break;
+                    case 404:
+                        paymentInformer.attr('data-content', `пользователь не найден`);
+                        paymentInformer.popover('show');
+                        break;
+                    case 407:
+                        paymentInformer.attr('data-content', `нельзя пополнить баланс самому себе`);
+                        paymentInformer.popover('show');
+                        break;
+                    case 409:
+                        paymentInformer.attr('data-content', `введена отрицательная сумма`);
+                        paymentInformer.popover('show');
+                        break;
+                    default:
+                        paymentInformer.attr('data-content', `Введены некорректные данные`);
+                        paymentInformer.popover('show');
+                        break;
+                }
+                updateUserBalance();
+            }
+        });
+
+    });
+})
