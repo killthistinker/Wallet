1831eeb87a601961a005ee632dbcfd97dad79134
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 15:42:18 +0600

    #1 Добавил viewModel для регистрации/вторизации

---
+++ Wallet/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wallet.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+        
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+        
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Wallet/Models/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wallet.Models.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [Remote("CheckAccountName", "AccountValidation", ErrorMessage = "Логин занят")]
+        public string UserName { get; set; }
+        
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Данные введены неверно")]
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [Remote("CheckAccountEmail", "AccountValidation", ErrorMessage = "Email занят")]
+        public string Email { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [DataType(DataType.Password)]
+        [Compare((nameof(Password)), ErrorMessage = "Пароли не совпадают")]
+        [Display(Name = "Подтвердить пароль")]
+
+        public string PasswordConfirm { get; set; }
+    }
+}
