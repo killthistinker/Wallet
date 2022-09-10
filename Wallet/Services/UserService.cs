71e9eff964203114fdf054ee4f941ec1f197b37c
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 16:50:25 +0600

    #2 Добавил сервис для паттерна репозиторий

---
+++ Wallet/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Wallet.Models.ViewModels;
+using Wallet.Repository.Interfaces;
+using Wallet.Services.Abstractions;
+
+namespace Wallet.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<UserViewModel> GetUser(int id)
+        {
+            var user = await _repository.GetFirstOrDefaultByIdAsync(id);
+            if (user is null) return null;
+            
+            UserViewModel model = new UserViewModel
+            {
+                UserBalance = user.Balance,
+                UserName = user.UserName
+            };
+
+            return model;
+        }
+    }
+}
