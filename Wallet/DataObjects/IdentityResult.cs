5bd99c1f70fdfd36a187b89dcead5261675b7184
Maxim Popovich <killthistinker@gmail.com>
2022-09-10 15:44:04 +0600

    #1 Создал модель для отображения ошибок

---
+++ Wallet/DataObjects/IdentityResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using StatusCodes = Wallet.Enums.StatusCodes;
+
+namespace Wallet.DataObjects
+{
+    public class IdentityResult
+    {
+        public List<string> ErrorMessages { get; set; }
+        public StatusCodes StatusCodes { get; set; }
+    }
+}
